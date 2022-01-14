d4cf97713ce886a80ea1e1608f2f4e1a4279050a
Antony Thumbi <agthumbi>
2022-01-14 15:06:01 +0300

    Added Add cart functionality

--- apis/cart.js
+++ apis/cart.js
@@ -3,19 +3,28 @@
 var api = {}
 
 let response = [{ code: 1, message: 'Something wento wrong.Please try again..' }]
-
+//Add Cart function
 api.getAddCart = async (req, res) => {
+    let request = `productid,isGuest,'session_id',p_qty`
+    const payload = `productid,isGuest,session_id,p_qty`.split(',');
+    let notifications = '';
+    for (let field of payload) {
+        if (req.body[field] === undefined || req.body[field] === '')
+            notifications += `${field} cannot be blank.Please enter a valid ${field}\n`;
+        request = request.replace(field, req.body[field])
+    }
+    if (notifications != '')
+        return res.send([{ code: 1, message: notifications }])
 
-    console.log(req.body)
     const sqlq = require('../config/rawsql')
     const result = await sqlq.query(`call psp_add_cart_by_session_id(${request})`)
-    //let response = {}
 
     if (result != undefined)
         response = result
 
     res.send(response)
 }
+//Update Cart function
 api.getEditCart = async (req, res) => {
 
 
@@ -28,6 +37,7 @@ api.getEditCart = async (req, res) => {
 
     res.send(response)
 }
+//Remove Cart function
 api.getRemoveCart = async (req, res) => {
 
     console.log(req.body)
--- config/connect.js
+++ config/connect.js
@@ -2,7 +2,7 @@ const db = {}
 db.host= "localhost",
 db.user= "root",
 db.password= "NELLYBELLY",
-db.database= "becode",
+db.database= "paystack",
 db.port= 3306
 db.connectionLimit =0,
 
