deae38cb7c3cda6c8c66cadc63830ee283320f07
Antony Thumbi <agthumbi>
2022-01-18 22:43:55 +0300

    cleaned the APIs and did some error handling to them and against logic functions

--- apis/cart.js
+++ apis/cart.js
@@ -1,85 +1,105 @@
-
+const logic = require('../config/logic')
 
 var api = {}
+var response = {}
 
-let response = [{ code: 1, message: 'Something wento wrong.Please try again..' }]
 //Add Cart function
 api.getAddCart = async (req, res) => {
     let request = `productid,isGuest,'session_id',p_qty`
     const payload = `productid,isGuest,session_id,p_qty`.split(',');
     let notifications = '';
     for (let field of payload) {
-        if (req.body[field] === undefined || req.body[field] === '')
-            notifications += `${field} cannot be blank.Please enter a valid ${field}\n`;
+        notifications += logic.validatePayload(req.body, field)
         request = request.replace(field, req.body[field])
     }
     if (notifications != '')
-        return res.send([{ code: 1, message: notifications }])
+        return res.send([logic.response(2, notifications)])
+
+
 
     const sqlq = require('../config/rawsql')
-    const result = await sqlq.query(`call psp_add_cart_by_session_id(${request})`)
+    const result = await sqlq.query(`call psp_add_cart_by_session_id(${request});`)
 
-    if (result != undefined)
-        response = result
+    response = logic.validateResponse(result)
+
 
     res.send(response)
 }
 //Update Cart function
 api.getEditCart = async (req, res) => {
-
-
+    let request = `productid,isGuest,'session_id',qty`
+    const payload = `productid,isGuest,session_id,qty`.split(',');
+    let notifications = '';
+    for (let field of payload) {
+        notifications += logic.validatePayload(req.body, field)
+        request = request.replace(field, req.body[field])
+    }
+    if (notifications != '')
+        return res.send([logic.response(2, notifications)])
     const sqlq = require('../config/rawsql')
-    const result = await sqlq.query(`call psp_update_cart_by_session_id`)
-    // let response = {}
 
-    if (result != undefined)
-        response = result
+    const result = await sqlq.query(`call psp_update_cart_by_session_id (${request});`)
+    response = logic.validateResponse(result)
 
     res.send(response)
 }
 //Remove Cart function
 api.getRemoveCart = async (req, res) => {
-
     let request = `sessionid`
     const payload = `sessionid`.split(',');
-    let notifications = '';  
+    let notifications = '';
     for (let field of payload) {
-        if (req.params[field] === undefined || req.params[field] === '')
-            notifications += `${field} cannot be blank.Please enter a valid ${field}\n`;
+        notifications += logic.validatePayload(req.params, field)
         request = request.replace(field, req.params[field])
     }
     if (notifications != '')
-        return res.send([{ code: 1, message: notifications }])
+        return res.send([logic.response(2, notifications)])
     const sqlq = require('../config/rawsql')
-    const result = await sqlq.query(`call psp_delete_cart_by_session_id (${request})`)
-
-
-    if (result != undefined)
-        response = result
+    const result = await sqlq.query(`call psp_delete_cart_by_session_id (${request});`)
+    response = logic.validateResponse(result)
 
     res.send(response)
 }
+//View Cart function
 api.getViewCart = async (req, res) => {
+    let totalAmount = 0
+    let request = `sessionid`
+    const payload = `sessionid`.split(',');
+    let newResult = []
+    let notifications = '';
+    for (let field of payload) {
+        notifications += logic.validatePayload(req.params, field)
+        request = request.replace(field, req.params[field])
+    }
 
-    console.log(req.body)
+    if (notifications != '')
+        return res.send([logic.response(2, notifications)])
     const sqlq = require('../config/rawsql')
-    const result = await sqlq.query(`call psp_view_cart_by_session_id`)
-    //let response = {}
+    let details = await sqlq.query(`call psp_view_cart_by_session_id (${request});`)
 
-    if (result != undefined)
-        response = result
+    response = logic.validateResponse(details)
+    if (details.length > 0 && details[0].amount != undefined) {
+        totalAmount = details.reduce((accum, item) => accum + item.amount, 0)
+        response = { totalAmount: totalAmount, details }
+    }
+
 
     res.send(response)
 }
+//view checkout function
 api.getCheckout = async (req, res) => {
-
-    console.log(req.body)
+    let request = `sessionid`
+    const payload = `sessionid`.split(',');
+    let notifications = '';
+    for (let field of payload) {
+        notifications += logic.validatePayload(req.params, field)
+        request = request.replace(field, req.params[field])
+    }
+    if (notifications != '')
+        return res.send([logic.response(2, notifications)])
     const sqlq = require('../config/rawsql')
-    const result = await sqlq.query(`call psp_checkout_cart_by_session_id`)
-    // let response = {}
-
-    if (result != undefined)
-        response = result
+    const result = await sqlq.query(`call psp_checkout_cart_by_session_id (${request})`)
+    response = logic.validateResponse(result)
 
     res.send(response)
 }
--- apis/products.js
+++ apis/products.js
@@ -1,45 +1,48 @@
-
+const logic = require('../config/logic')
 
 var api = {}
 
-let response = [{ code: 1, message: 'Record(s) do not exists' }]
+var response={}
 
 api.getProductsCategory = async (req, res) => {
 
-   
     const sqlq = require('../config/rawsql')
-    const result = await sqlq.query('call becode.psp_get_categories();')
-
-
-    if (result != undefined)
-        response = result
+    const result = await sqlq.query('call psp_get_categories();')
+    response = logic.validateResponse(result)
 
     res.send(response)
 }
 api.getProductsByCategoryID = async (req, res) => {
-
- 
+    let request = `categoryid`
+    const payload = `categoryid`.split(',');
+    let notifications = '';
+    for (let field of payload) {
+        notifications += logic.validatePayload(req.params, field)
+        request = request.replace(field, req.params[field])
+    }
+    if (notifications != '')
+        return res.send([logic.response(2, notifications)])
+        
     const categoryid = req.params.categoryid
     const sqlq = require('../config/rawsql')
-    const result = await sqlq.query(`call psp_get_products_by_categoryId(${categoryid})`)
-    let response = {}
-
-    if (result != undefined)
-        response = result
-
+    const result = await sqlq.query(`call psp_get_products_by_categoryId(${request})`)
+    response = logic.validateResponse(result)
     res.send(response)
 }
 api.getProductByProductID = async (req, res) => {
+    let request = `productid`
+    const payload = `productid`.split(',');
+    let notifications = '';
+    for (let field of payload) {
+        notifications += logic.validatePayload(req.params, field)
+        request = request.replace(field, req.params[field])
+    }
+    if (notifications != '')
+        return res.send([logic.response(2, notifications)])
 
-    const productid = req.params.productid
     const sqlq = require('../config/rawsql')
-    const result = await sqlq.query(`call psp_get_product_by_product_id (${productid})`)
-    let response = {}
-
-    if (result === undefined)
-        response = [{ code: 1, message: 'Record(s) do not exists' }]
-    else
-        response = result
+    const result = await sqlq.query(`call psp_get_product_by_product_id (${request})`)
+    response = logic.validateResponse(result)
 
     res.send(response)
 }
