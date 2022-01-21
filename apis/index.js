b927d10cd7424382145d5d2cf46104e8f8d27572
Antony Thumbi <agthumbi>
2022-01-21 20:07:43 +0300

    added authentication roter

--- apis/index.js
+++ apis/index.js
@@ -1,26 +1,26 @@
-
-var cartapi=require('./cart')
-var productsapi=require('./products')
+var authenticate = require('./auth')
+var cartapi = require('./cart')
+var productsapi = require('./products')
 
 
 
 
 
 var extend = require('util')._extend
- var iapi={}
-  iapi.index=(req, res)=>{
-    res.send('Welcome to PayStack api');
+var iapi = {}
+iapi.index = (req, res) => {
+  res.send('Welcome to PayStack api');
 }
- iapi.status=(req, res)=>{
-    res.send('0');
+iapi.status = (req, res) => {
+  res.send('0');
 }
 
-
-var api=extend(iapi,cartapi)
- api=extend(api,productsapi)
-
-
+var api = extend(iapi, authenticate)
+api = extend(api, cartapi)
+api = extend(api, productsapi)
 
 
 
-module.exports=api
+
+
+module.exports = api
